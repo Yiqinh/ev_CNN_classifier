9e88e3594cb0f48b4e377a37ef56911c8bc2e296
Yiqin <yiqinhuang@berkeley.edu>
2024-08-25 21:22:27 -0700

    react almost done

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,10 +1,64 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+import tensorflow as tf
+import os
+import cv2
+import imghdr
+import numpy as np
+import math
+
+from werkzeug.utils import secure_filename
+
+here = os.path.dirname(os.path.abspath(__file__))    
+model = tf.keras.models.load_model(os.path.join(os.path.dirname(here), 'pineapple_CNN.keras'))
+
+cur_image = None
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World"
+@app.route("/cnn", methods=['POST'])
+def upload():
+    d = {}
+    try:
+        file = request.files['file_from_react']
+        filename = file.filename
+        print(f"Uploading file {filename}")
+        d['status'] = 'OPEN SEASAME'
+
+        filename=secure_filename(file.filename)
+        image_path="./images/" + filename
+
+        file.save(image_path)
+        print(imghdr.what(image_path))
+        cur_image = image_path
+
+    except Exception as e:
+        print(f"Couldn't upload file {e}")
+        d['status'] = 0
+
+    return jsonify(d)
+
+@app.route("/infer", methods=['POST'])
+def infer():
+    d = {}
+    img = cv2.imread(cur_image)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    resize = tf.image.resize(img, (256,256))
+    yhat = model.predict(np.expand_dims(resize/255, 0))
+   
+    if yhat > 0.5:
+        y_pred = "Pineapple"
+    else:
+        y_pred = "No Pineapple"
+
+    yhat = float(yhat[0][0])
+    yhat = max(yhat, 1-yhat)
+    yhat = math.floor(yhat * 100) / 100
+    
+    #clean up
+    os.remove(cur_image)
+
+    return jsonify(d)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- react_client/src/App.js
+++ react_client/src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
 
 function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const imageRef = useRef();
+
+  const [results, setResults] = useState([]);
+
+  const uploadImage = async (e) => {
+    const { files } = e.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(files[0]);
+      setImageUrl(url);
+
+      const file = e.target.files[0];
+      const data = new FormData();
+      data.append("file_from_react", file);
+
+      let response = await fetch("/cnn", {
+        method: "post",
+        body: data,
+      });
+      let res = await response.json();
+
+      console.log(res.status);
+      if (res.status !== 'OPEN SEASAME') {
+        alert("Error uploading file");
+      }
+    } else {
+      setImageUrl(null);
+    }
+  };
+
+  const inference = async (e) => {
+    let response = await fetch("/infer", {
+      method: "post",
+      body: "perform inference",
+    });
+    let res = await response.json();
+  }
+
+  const CnnResult = ({ is_pineapple, confidence, isTopResult }) => (
+    <div className={`pineapple-result ${isTopResult ? "top-result" : ""}`}>
+      <div className="pineapple-header">
+        <span className="pizza-name">{is_pineapple.toUpperCase()}</span>
+        {isTopResult && <span className="best-guess">Best Guess</span>}
+      </div>
+      <div className="confidence-level">
+        Confidence level: {confidence.toFixed(2)}%
+      </div>
+    </div>
+  );
+
+  const PizzaTypeResults = ({ results }) => (
+    <div className="inference-results">
+      {results.map((result, index) => (
+        <CnnResult
+          key={result.pizza}
+          is_pineapple={result.pizza}
+          confidence={result.confidence}
+          isTopResult={result.top}
+        />
+      ))}
+    </div>
+  );
+
+  const sampleResults = [
+    { pizza: "Pineapple", confidence: 24.47, top: false },
+    { pizza: "No Pineapple", confidence: 38.37, top: true },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">Pineapple Pizza Classification</h1>
+      <div className="inputHolder">
+        <input
+          type="file"
+          accept="image/jpg, image/jpeg, image/png"
+          className="uploadInput"
+          onChange={uploadImage}
+        />
+      </div>
+      <div className="mainWrapper">
+        <div className="mainContent">
+          <div className="imageHolder">
+            {imageUrl && (
+              <img
+                src={imageUrl}
+                alt="Upload Preview"
+                crossOrigin="anonymous"
+                ref={imageRef}
+              />
+            )}
+          </div>
+          <PizzaTypeResults results={sampleResults} />
+        </div>
+        {imageUrl && <button className="button">Identify Image</button>}
+      </div>
     </div>
   );
 }
